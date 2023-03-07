af772ba9d96ab55499ddba446ecc515748f19789
Ryan Peabody <rpeabody43@gmail.com>
2023-03-06 19:13:34 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <SDL.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) > 0) 
+		cout << "SDL FAILED TO INITIALIZE: " << SDL_GetError() << endl;
+
+	cout << "test" << endl;
+	
+
+	SDL_Quit();
+	return 0;
+}
